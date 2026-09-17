6f92e7cd2239d6c72c7e8b9339ebee36da730e1e
PABA <PABA@users.noreply.github.com>
2026-09-17 00:01:54 -0500

    refactor(ui): remove dead SearchableSelect component (UX-9 R-11)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,19 +1,18 @@
-export { Button } from './button';
-export { Input } from './input';
-export { Card } from './card';
-export { Modal } from './modal';
-export { Select } from './select';
-export { Icon } from './icon';
-export { Badge } from './badge';
-export { ConfirmDialog } from './confirm-dialog';
+export { Button } from "./button";
+export { Input } from "./input";
+export { Card } from "./card";
+export { Modal } from "./modal";
+export { Select } from "./select";
+export { Icon } from "./icon";
+export { Badge } from "./badge";
+export { ConfirmDialog } from "./confirm-dialog";
 export {
   EntityDeleteButton,
   type EntityDeleteAction,
   type DeleteActionResult,
-} from './entity-delete-button';
-export { ActionIconButton } from './action-icon-button';
-export { Skeleton } from './skeleton';
-export { EmptyState } from './empty-state';
-export { SearchableSelect } from './searchable-select';
-export { Toast, type ToastVariant } from './toast';
-export { ToastProvider, useToastContext } from './toast-provider';
+} from "./entity-delete-button";
+export { ActionIconButton } from "./action-icon-button";
+export { Skeleton } from "./skeleton";
+export { EmptyState } from "./empty-state";
+export { Toast, type ToastVariant } from "./toast";
+export { ToastProvider, useToastContext } from "./toast-provider";
